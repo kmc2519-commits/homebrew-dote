e394d090345d8847d3a96d38ceea691411c62e60
kmc2519-commits <kmc2519@kamiyama.ac.jp>
2025-11-03 20:34:28 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/kmc2400/homebrew-dote"
   url "https://raw.githubusercontent.com/kmc2519-commits/homebrew-dote/main/dote.c"
-  sha256 "fb310aa93ad14e22ecf8ae77da7b06df5d8159d597b3b97b83f7bc167e3044d8"
+  sha256 "40b929c996b07eb45582f343b0a7a061947e684ff5defddda5dffbe7dc3a3f3b"
   version "1.0.0"
 
 
